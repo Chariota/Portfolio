ebb9bc24d7699d53a00f8825fad7e0444dbed0e0
Chariota <114934167+Chariota@users.noreply.github.com>
2023-10-10 14:09:01 +0700

    Update app.js

--- โปรแกรมทายหัวก้อย/app.js
+++ โปรแกรมทายหัวก้อย/app.js
@@ -1,4 +1,5 @@
 let round = prompt("จะเล่นทั้งหมดกี่รอบ")
+let sum = 0
 for (let i = 0; i < round; i++) {
     let ans = prompt("หัว หรือ ก้อย!")
     let random_ans = ""
@@ -13,10 +14,12 @@ for (let i = 0; i < round; i++) {
 
     if(ans == random_ans) {
         alert("ยินดีด้วยคุณตอบถูก")
+        sum += 1
     } 
     else{
         alert("เสียใจด้วยคุณตอบผิด")
     }
     console.log(random_ans + ":" + ans)
     document.getElementById("game-list").innerHTML += ans + ":" + random_ans + "<br>" 
+    document.getElementById("result").innerHTML = "คุณตอบถูกทั้งหมด " + sum + " รอบ"
 }
